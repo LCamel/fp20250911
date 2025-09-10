4a2eab2a3c01ecee45b70245df8dc70749b2f2b3
Luoh Ren-Shan (LCamel) <lcamel@gmail.com>
2025-09-10 16:41:38 +0800

    add course-materials/

---
+++ course-materials/practicals_02-1_code/TChallenge.hs
@@ -0,0 +1,11 @@
+import Prelude ()
+import MiniPrelude
+import MChallenge
+import Test.QuickCheck
+
+{- Your code here -}
+
+{- Test your code using quickCheck -}
+
+correct0 :: (List Int -> List Int -> List (List Int)) -> List Int -> List Int -> Bool
+correct0 f xs ys = f xs ys == find xs ys
